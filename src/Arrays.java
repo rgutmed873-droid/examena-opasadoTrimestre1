34f45b43c059154e8be6b4783d20486fc210405a
rafag <rgutmed873@iesgrancapitan.org>
2025-12-02 17:45:01 +0100

    Ejercicio array hecho y matrices solo inicializado y mostrado

--- src/Arrays.java
+++ src/Arrays.java
@@ -5,8 +5,33 @@ public class Arrays {
 
     int [] array = new int [10];
     inicializarArray(array);
+    sumaArray(array);
 
+    }
 
+    /**
+     * Metodo para realizar la suma de una array en el que los dos elementos más grandes den la suma máxima
+     * Junto con la suma de todos los otros valores
+     * @param array
+     */
+    private static void sumaArray(int[] array) {
+        int suma = 0;
+        int sumaMax = 0;
+        int valor1 = 0;
+        int valor2 = 0;
+        for (int i = 0; i < array.length; i++) {
+            for (int j = i+1; j < array.length; j++) {
+                suma = array[i] + array[j];
+                if (suma > sumaMax) {
+                    sumaMax = suma;
+                    valor1 = array[i];
+                    valor2 = array[j];
+
+                }
+                System.out.println("suma: " + suma);
+            }
+        }
+        System.out.println("Los dos elementos del array son " + valor1 + " " + valor2 + " la suma máxima es: " + sumaMax);
     }
 
     private static void inicializarArray(int[] array) {
---
+++ src/Matrices.java
@@ -0,0 +1,27 @@
+import java.util.Random;
+
+public class Matrices {
+   public  static void main(String[] args) {
+
+       int [] [] matriz = new int[5][5];
+
+        inicializarMatriz(matriz);
+   }
+
+    private static void inicializarMatriz(int[][] matriz) {
+        for (int i = 0; i < matriz.length; i++) {
+            for (int j = 0; j < matriz[0].length; j++) {
+                matriz[i][j] = generarNumerosAleatorios(0,100);
+
+                System.out.print(" | " +  matriz[i][j] + " | ");
+            }
+            System.out.println();
+        }
+    }
+
+
+    private static int generarNumerosAleatorios(int min, int max){
+       Random rand = new Random();
+       return rand.nextInt(max-min+1)+min;
+   }
+}
